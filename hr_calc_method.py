c7dfc5b3ba9c93c8fa9baad88b36225fb8a59cc9
Federico Pantanella <federico.pantanella.98@gmail.com>
2022-03-20 17:52:53 +0100

    complete debug

--- hr_calc_method.py
+++ hr_calc_method.py
@@ -10,7 +10,6 @@ class hrMethod():
 
         # Foresee possibility to include object with all parameters to include
         def __init__(self, fs):
-            self.i = imagpart
             self.refPeriod = 0.250
             self.THRES = 0.6
             self.fs = fs
@@ -27,7 +26,7 @@ class hrMethod():
             self.MAX_FORCE = np.array([])
             self.MIN_AMP = 0.1
         
-        def inputECG(ecg):
+        def inputECG(self, ecg):
             """
             A function to rsave ecg data, number of samples and correspondent time.
             
@@ -50,7 +49,7 @@ class hrMethod():
 
             self.time = np.arange(1/self.fs, ceil(self.num_samples/self.fs), 1/self.fs)
 
-        def filter_band_pass():
+        def filter_band_pass(self):
             """
             Filter the ecg with a sombrero low pass filter.
             """
@@ -65,9 +64,9 @@ class hrMethod():
                     -2.357742589814283e-04, -7.757327341237223e-05]
             # Is resampling of the filter necessary?
             # b1 = sig.resample(b1,len(b1)/(self.fs/250))
-            self.filt_ecg = sig.filtfilt(b1,1,self.ecg)
+            self.filt_ecg = sig.filtfilt(b1,1,self.ecg[:,0])
 
-        def calculateHR():
+        def calculateHR(self):
             """
             Main function to calculate HR.
             """
@@ -84,7 +83,7 @@ class hrMethod():
                 en_thres = np.array([])
             else:
                 diffECG = np.diff(self.filt_ecg)
-                sqrECG = np.matmul(diffECG,diffECG)
+                sqrECG = np.multiply(diffECG,diffECG)
                 intECG = sig.lfilter(np.ones(self.INT_NB_COEFF),1,sqrECG)
                 mdfint = sig.medfilt(intECG,kernel_size=self.MED_SMOOTH_NB_COEFF)
                 delay = ceil(self.INT_NB_COEFF/2)
@@ -110,69 +109,83 @@ class hrMethod():
                     ind_xs = ceil(99/100*xs.size)
                     en_thres = xs[ind_xs]
 
-            poss_reg =mdfint>(self.THRES * en_thres)
+            poss_reg_bool =mdfint>(self.THRES * en_thres)
 
-            if poss_reg.size==0:
-                poss_reg[9] = 1
+            if poss_reg_bool.size==0:
+                poss_reg_bool[9] = 1
 
             if self.SEARCH_BACK:
-                indAboveThreshold = np.where(poss_reg)[0]
+                indAboveThreshold = np.where(poss_reg_bool)[0]
                 RRv = np.diff(self.time[indAboveThreshold])
                 medRRv = np.median(RRv[np.where(RRv>0.01)[0]])
                 indMissedBeat = np.where(RRv>(1.5*medRRv))[0]
-                indStart = indAboveThreshold(indMissedBeat)
-                indEnd = indAboveThreshold(indMissedBeat+1)
+                indStart = indAboveThreshold[indMissedBeat]
+                indEnd = indAboveThreshold[indMissedBeat+1]
 
                 for i in range(0,len(indStart)):
-                    poss_reg[indStart[i]:indEnd[i]] = self.mdfint[indStart[i]:indEnd[i]] > (0.5*self.THRES*en_thres)
+                    poss_reg_bool[indStart[i]:indEnd[i]] = mdfint[indStart[i]:indEnd[i]] > (0.5*self.THRES*en_thres)
 
-            left = np.where(np.diff(np.append([0],poss_reg) == 1))[0]
-            right = np.where(np.diff(np.append(poss_reg,[0]) == -1))[0]
+            poss_reg = np.zeros([poss_reg_bool.size])
+            for i in range(0,poss_reg_bool.size):
+                poss_reg[i] = int(poss_reg_bool[i]==True)
 
-            if self.signForce[0]:
+            left = np.where(np.diff(np.append([0],poss_reg)) == 1)[0]
+            right = np.where(np.diff(np.append(poss_reg,[0])) == -1)[0]
+
+            if self.signForce.size != 0:
                 sign = self.signForce[0]
             else:
-                nb_s = len(left<30*fs)
+                nb_s = len(left<30*self.fs)
                 loc = np.zeros([nb_s])
                 for j in range(0,nb_s):
-                    loc[j] = np.argmax(abs(self.filt_ecg[left[j]:right[j]]))
-                    loc[j] = loc[j] -1 + left[j]
-                sign = np.mean(self.ecg[loc])
+                    loc[j] = int(np.argmax(abs(self.filt_ecg[left[j]:right[j]+1])) -1 +left[j])
+                    #loc[j] = int(loc[j] -1 + left[j])
+
+                tot=0
+                for j in range(0,len(loc)):
+                    tot = tot + self.ecg[int(loc[j])]
+                sign = tot/(len(loc))
             
             #Loop through all possibilities
-            compt = 1
+            compt = 0
             NB_PEAKS = np.size(left)
             maxval = np.zeros([NB_PEAKS])
             maxloc = np.zeros([NB_PEAKS])
             for i in range(0,NB_PEAKS):
                 if sign > 0:
-                    maxloc[compt] = np.argmax(self.ecg[left[i]:right[i]])
-                    maxval[compt] = ecg[maxloc[compt]]
+                    maxloc[compt] = np.argmax(self.ecg[left[i]:right[i]+1])
+                    maxval[compt] = self.ecg[maxloc[compt]]
                 else:
-                    maxloc[compt] = np.argmin(self.ecg[left[i]:right[i]])
-                    maxval[compt] = ecg[maxloc[compt]]
+                    maxloc[compt] = np.argmin(self.ecg[left[i]:right[i]+1])
+                    maxval[compt] = self.ecg[int(maxloc[compt])]
                 maxloc[compt] = maxloc[compt] -1 + left[i] # add offset of present location
 
             # Refractorz period --> improves results
-                if compt > 1:
-                    if (maxloc[compt] - maxloc[compt-1] < (self.fs * self.refPeriod)) and (abs(maxval(compt)) < abs(maxval(compt-1))):
-                        np.delete(maxloc[compt])
-                        np.delete(maxval[compt])
-                    elif (maxloc[compt] - maxloc[compt-1] < (self.fs * self.refPeriod)) and (abs(maxval(compt)) >= abs(maxval(compt-1))):
-                        np.delete(maxloc[compt-1])
-                        np.delete(maxval[compt-1])
+                if compt > 0:
+                    if (maxloc[compt] - maxloc[compt-1] < (self.fs * self.refPeriod)) and (abs(maxval[compt]) < abs(maxval[compt-1])):
+                        maxloc[compt] = -1
+                        maxval[compt] = -10
+                    elif (maxloc[compt] - maxloc[compt-1] < (self.fs * self.refPeriod)) and (abs(maxval[compt]) >= abs(maxval[compt-1])):
+                        maxloc[compt] = -1
+                        maxval[compt] = -10
                     else:
                         compt=compt+1
                 else:
                     compt=compt+1
+
+            idx = np.where(maxloc==-1)[0]
+            maxloc = np.delete(maxloc,idx)
+            maxval = np.delete(maxval,idx)
             self.qrs_pos = maxloc # datapoints qrs positions
-            self.R_t = self.time[maxloc] #timestamps QRS positions
+            self.R_t = np.zeros([maxloc.size]) #timestamps QRS positions
+            for i in range(0,maxloc.size):
+                self.R_t[i] = self.time[int(maxloc[i])] 
             self.R_amp = maxval # amplitude at QRS positions
             self.hrv = 60*np.ones([len(self.R_t)-1]) / (np.diff(self.R_t))
 
             return self.qrs_pos, sign, en_thres
 
-        def plotDebug():
+        def plotDebug(self):
             fig, ax = plt.subplots(3, 1, figsize = (10,6), sharex=True, num = 0)
             ax[0].plot(self.time, self.ecg, 'b', zorder = 0, label="raw ecg")
             ax[0].plot(self.time, self.filt_ecg, 'r', zorder=5, label="filtered ecg")
--- test_script.py
+++ test_script.py
@@ -3,8 +3,16 @@ import os
 import csv
 import pandas as pd
 import wfdb
+import hr_calc_method as hr
 
 record = wfdb.rdrecord('14046')
 signals, fields = wfdb.rdsamp('14046', channels=[0], sampfrom=100, sampto=15000)
-wfdb.plot_wfdb(record=record, title='Example signals')
+ecg = np.zeros([signals.shape[0],1])
 
+for i in range(signals.shape[0]):
+    ecg[i] = signals[i]
+#wfdb.plot_wfdb(record=record, title='Example signals')
+method = hr.hrMethod(128)
+method.inputECG(ecg)
+method.calculateHR()
+print(method)
